bcdd55c7d8ea623b72bc86c7a5413a5f85ec2f93
PauloHenriqueBentoDosSantos <phbento@outlook.com.br>
2025-01-03 12:54:22 -0300

    Update event date in main.js to June 23, 2025

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
     AOS.init();
 
-    const dataDoEvento = new Date("Jun 23, 2024 19:00:00");
+    const dataDoEvento = new Date("Jun 23, 2025 19:00:00");
     const timeStampDoEvento = dataDoEvento.getTime();
 
     const contaAsHoras = setInterval(() => { 
